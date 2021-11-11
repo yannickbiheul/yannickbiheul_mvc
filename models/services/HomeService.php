9e3728218f9b4bddf06665e023245c0963651d8e
Yannick B <yannickbiheul@gmail.com>
2021-11-11 14:49:10 +0100

    Ajout vérifications email

--- models/services/HomeService.php
+++ models/services/HomeService.php
@@ -16,17 +16,31 @@ class HomeService {
         if (isset($post['nom']) && isset($post['email']) && isset($post['texte'])) {
 
             $message = [];
+            $email = htmlspecialchars($post['email']);
+
+            function isRussian($email) {
+                $mail = htmlspecialchars($_POST['email']);
+                $arrayMail = str_split($mail);
+                $countLetters = count($arrayMail);
+                $lastLetters = $arrayMail[$countLetters - 2] . $arrayMail[$countLetters - 1];
+                
+                return ($lastLetters == "ru");
+            }
+
+            if (isRussian($email)) {
+                $errors = "Mail invalide";
+            } else {
+                if (strlen($post['email']) < 3) {
+                    $errors = "Il me faut plus de 3 lettres pour l'email !";
+                } else {
+                    array_push($message, htmlspecialchars($post['email']));
+                }
+            }
 
             if (strlen($post['nom']) < 3) {
                 $errors = "Il me faut plus de 3 lettres pour le nom !";
             } else {
                 array_push($message, htmlspecialchars($post['nom']));
-            }
-
-            if (strlen($post['email']) < 3) {
-                $errors = "Il me faut plus de 3 lettres pour l'email !";
-            } else {
-                array_push($message, htmlspecialchars($post['email']));
             }
 
             if (strlen($post['texte']) < 3) {
